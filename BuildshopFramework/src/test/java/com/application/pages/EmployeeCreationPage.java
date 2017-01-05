edb9ede640c3482f8fcd6687d77e913eabcef467
Divyashree <divyarajj1994@gmail.com>
2017-01-05 19:20:12 +0530

    verify

--- BuildshopFramework/src/test/java/com/application/pages/EmployeeCreationPage.java
+++ BuildshopFramework/src/test/java/com/application/pages/EmployeeCreationPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
 public class EmployeeCreationPage
-{
+{//commented 
 	public EmployeeCreationPage(WebDriver driver)
 	{
 		
