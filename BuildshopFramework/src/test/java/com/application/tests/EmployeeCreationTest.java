3f9818d54aef71ff01a78f885d966c8e2e9eb2c6
Divyashree <divyarajj1994@gmail.com>
2017-01-06 13:29:06 +0530

    Employee Creation Test

---
+++ BuildshopFramework/src/test/java/com/application/tests/EmployeeCreationTest.java
@@ -0,0 +1,19 @@
+package com.application.tests;
+
+import org.testng.annotations.Test;
+import com.application.pages.EmployeeCreationPage;
+import com.application.pages.LoginPage;
+
+public class EmployeeCreationTest extends BaseClass
+{
+    @Test
+    public void testEmployeeCreation()
+    {
+    	LoginPage login=new LoginPage(driver);
+	    login.loginToApplication();
+	    EmployeeCreationPage emp=new EmployeeCreationPage(driver);
+    	emp.createUser();		
+    }
+    
+
+}
