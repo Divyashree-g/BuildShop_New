b1a1be40034b5eb1606a8d8a94923773f3a9e488
Divyashree <divyarajj1994@gmail.com>
2017-01-09 18:08:31 +0530

    Employee creation page

--- BuildshopFramework/src/test/java/com/application/pages/EmployeeCreationPage.java
+++ BuildshopFramework/src/test/java/com/application/pages/EmployeeCreationPage.java
@@ -12,6 +12,7 @@ public class EmployeeCreationPage
 	{
 		PageFactory.initElements(driver, this);
 	}
+	
 	@FindBy(id="ctl00_LoginViewProfessional_LoginName1")
 	private WebElement userNameDropDown;
 	
@@ -25,28 +26,43 @@ public class EmployeeCreationPage
 	private WebElement userTypeDropDown;
 	
 	@FindBy(id="ctl00_MainContent_CreateUserWizard1_CreateUserStepContainer_UserName")
-	private WebElement userNameTextField;
+	private WebElement userNameTextField1;
 	
 	@FindBy(id="ctl00_MainContent_CreateUserWizard1_CreateUserStepContainer_Email")
-	private WebElement emailTextField;
+	private WebElement emailTextField1;
 	
 	@FindBy(id="ctl00_MainContent_CreateUserWizard1_CreateUserStepContainer_Password")
-	private WebElement passwordTextField;
+	private WebElement passwordTextField1;
 	
 	@FindBy(id="ctl00_MainContent_CreateUserWizard1___CustomNav0_StepNextButtonButton")
 	private WebElement createUserButton;
 	
+	@FindBy(id="ctl00_MainContent_CreateUserWizard1_CompleteStepContainer_ContinueButtonButton")
+	private WebElement continueButton;
+	
+	
 	public void createUser()
 	{
 		userNameDropDown.click();
+		
 		manageUsersLink.click();
 		addUsersLink.click();
 		Select select = new Select(userTypeDropDown);
 		select.selectByVisibleText("Employee");
-		userNameTextField.sendKeys("Devis");
-		emailTextField.sendKeys("divyarajj1994@gmail.com");
-		passwordTextField.sendKeys("Devis1234");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		userNameTextField1.click();
+		userNameTextField1.sendKeys("Aana rai");
+		emailTextField1.click();
+		emailTextField1.sendKeys("aana1234@gmail.com");
+		passwordTextField1.click();
+		passwordTextField1.sendKeys("aana12345");
 		createUserButton.click();
+		continueButton.click();
 	}	
 	
 	
