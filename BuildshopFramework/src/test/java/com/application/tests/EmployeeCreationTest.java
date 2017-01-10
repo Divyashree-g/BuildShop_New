29069657fdfb935d514fbee4b477f2a36e03074b
Divyashree <divyarajj1994@gmail.com>
2017-01-10 18:24:44 +0530

    Emp

--- BuildshopFramework/src/test/java/com/application/tests/EmployeeCreationTest.java
+++ BuildshopFramework/src/test/java/com/application/tests/EmployeeCreationTest.java
@@ -1,6 +1,8 @@
 package com.application.tests;
 
 import org.testng.annotations.Test;
+
+import com.application.libraries.ExcelLibrary;
 import com.application.pages.EmployeeCreationPage;
 import com.application.pages.LoginPage;
 
@@ -11,9 +13,14 @@ public class EmployeeCreationTest extends BaseClass
     public void testEmployeeCreation()
     {
     	LoginPage login=new LoginPage(driver);
-	    login.loginToApplication(driver);
-	    /*EmployeeCreationPage emp=new EmployeeCreationPage(driver);
-    	emp.createUser();	*/	
+    	String username = ExcelLibrary.getExcelData("./data/data.xlsx", "Sheet1", 1, 1);
+		String password = ExcelLibrary.getExcelData("./data/data.xlsx", "Sheet1", 1, 2);
+	    login.loginToApplication(driver, username, password);
+	    EmployeeCreationPage emp=new EmployeeCreationPage(driver);
+	    String empusername= ExcelLibrary.getExcelData("./data/data.xlsx", "Emp-Creation", 1, 0);
+	    String empemail= ExcelLibrary.getExcelData("./data/data.xlsx", "Emp-Creation", 1, 1);
+	    String emppassword= ExcelLibrary.getExcelData("./data/data.xlsx", "Emp-Creation", 1, 2);
+	    emp.createUser(empusername, empemail, emppassword);
     }
     
 
